32195d3269fb6564f7efd665b7a6d0a9a332635f
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-17 16:02:20 -0400

    lint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * to-arg <https://github.com/jonschlinkert/to-arg>
  *
- * Copyright (c) 2015 Jon Schlinkert.
- * Licensed under the MIT license.
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
